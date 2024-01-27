9b209df8bde038705000146400dcebba08f1db93
ramonPL14 <2101102020@student.buksu.edu.ph>
2024-01-27 13:53:38 +0800

    temp dashboard and management

--- src/pages/management/Management.jsx
+++ src/pages/management/Management.jsx
@@ -6,7 +6,38 @@ const Management = () => {
     return (
         <div>
             < HeaderAndSidebar />
-            <h1>Management</h1>
+            <section className="management">
+                <h1 className="heading">System Management</h1>
+                <div className="managementContainer">
+                    <div className="addThings">
+                    <a href="#" className="addLocation">Add Location</a>
+                    <a href="#" className="addDevice">Add Device</a>
+                    </div>
+                    <p className="recentlyAdded">Recently Added!</p>
+                    <div className="recentlyAddedContainer">
+                    {[...Array(4)].map((_, index) => (
+                        <div className="box" key={index}>
+                        <div className="boxDetails">
+                            <p>Location: Gaisano Malaybalay</p>
+                            <p>Location ID: 12637</p>
+                            <p>Sensor Name: Sensor 1</p>
+                            <p>Sensor ID: 8080</p>
+                            <div className="indented">
+                            <p>Particulate Matter2.5: 12ug/m3</p>
+                            <p>Particulate Matter10: 150ug/m3</p>
+                            <p>Carbon Monoxide: 30ppm</p>
+                            <p>Ozone: 300 DU</p>
+                            <p>Nitrogen Dioxide: 200ug/m3</p>
+                            </div>
+                        </div>
+                        <div className="boxViewDetails">
+                            <a href="#" className='inline-btn'>View Details</a>
+                        </div>
+                        </div>
+                    ))}
+                    </div>
+                </div>
+            </section>
         </div>
 
     )
---
+++ src/pages/settings/Settings.jsx
@@ -0,0 +1,15 @@
+import './settings.css'; // Make sure to import the corresponding CSS file
+import HeaderAndSidebar from '../../components/headerAndSidebar/HeaderAndSidebar';
+
+
+const Settings = () => {
+    return (
+        <div>
+            < HeaderAndSidebar />
+            <h1>Settings</h1>
+        </div>
+
+    )
+}
+
+export default Settings;
