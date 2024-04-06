8fec169e2465c6a027ee9791b5b6415988e38b29
ramzel1414 <ramonpaulocaumban@gmail.com>
2024-04-06 22:32:22 +0800

    update csv btn

--- resources/views/charts/monitoring/co.blade.php
+++ resources/views/charts/monitoring/co.blade.php
@@ -4,9 +4,9 @@
         <div class="row">
           <div class="col-2 d-flex justify-content-between w-100">
             <h6 class="d-flex align-items-center">CO</h6>
+            <button id="expCO" class="btn btn-outline-secondary btn-icon-text me-2 mb-2 mb-md-0">Download CSV</button>
           </div>
           <div id="co" class="mt-md-3 mt-xl-0 order-last"></div>
-          <button id="expCO">Export as csv</button>
         </div>
       </div>
     </div>
--- resources/views/charts/monitoring/no2.blade.php
+++ resources/views/charts/monitoring/no2.blade.php
@@ -4,9 +4,9 @@
         <div class="row">
           <div class="col-2 d-flex justify-content-between w-100">
             <h6 class="d-flex align-items-center">NO2</h6>
+            <button id="expNO2" class="btn btn-outline-secondary btn-icon-text me-2 mb-2 mb-md-0">Download CSV</button>
           </div>
           <div id="no2" class="mt-md-3 mt-xl-0 order-last"></div>
-          <button id="expNO2">Export as csv</button>
         </div>
       </div>
     </div>
