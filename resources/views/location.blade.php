949ee2fce92b4da39ee05da8ad17e58e99f37f2a
OrangeMintz <Nickzgacus@gmail.com>
2024-04-13 15:03:09 +0800

    fixed map size

--- resources/views/location.blade.php
+++ resources/views/location.blade.php
@@ -45,7 +45,7 @@
             <div class="d-flex justify-content-center">
                 <p class="mb-3" id="locationTitle">Device Locations</p>
             </div>
-            <div id="map" class="rounded-3" style="height: 500px"></div>
+            <div id="map" class="rounded-3" style="height: 700px"></div>
         </div>
     </div>
 
