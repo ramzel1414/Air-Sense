65dc825ce943bc39f0ad094c2efc5e95381c514a
OrangeMintz <Nickzgacus@gmail.com>
2024-04-24 22:57:58 +0800

    fixed smol issue

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -73,7 +73,7 @@
 
 
         <h2>AirSense</h2>
-        
+
         {{-- Tabs for Monitoring and Forecasting--}}
         <ul class="nav nav-tabs nav-tabs-line" id="lineTab" role="tablist">
             <li class="nav-item">
@@ -97,9 +97,9 @@
                 <h5>Bukidnon State University</h5>
                 <div class="text-status d-flex gap-2 align-items-center">
                     <div>STATUS:</div>
-                    <div class="device-status">
-                        <div class="active-circle"></div>
-                        <div class="">ACTIVE</div>
+                    <div class="device-status-online">
+                        <div class="status-circle"></div>
+                        <div class="">ONLINE</div>
                     </div>
                 </div>
             </div>
