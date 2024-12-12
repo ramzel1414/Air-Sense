1ef9320ef5091d54407b9287e7001427dfe98e09
OrangeMintz <Nickzgacus@gmail.com>
2024-12-12 22:58:20 +0800

    - refactors signatories section - refactors signatories section and pollutant information

--- app/Http/Controllers/PdfControllerPM10Filter.php
+++ app/Http/Controllers/PdfControllerPM10Filter.php
@@ -5,8 +5,8 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Reports\CoverPage;
 use App\Http\Controllers\Reports\PdfReport;
 use App\Http\Controllers\Reports\PM10\PM10Info;
+use App\Http\Controllers\Reports\SignatorySection;
 use App\Models\AirQualityData;
-use App\Models\Signatory;
 use Illuminate\Support\Facades\DB;
 use Carbon\Carbon;
 
@@ -132,73 +132,11 @@ class PdfControllerPM10Filter extends Controller
             $fpdf->Cell(40, 10, $classification, 1, 1, 'C', true); // Add classification with background color
         }
 
-        // Fetch signatory data
-        $signatories = Signatory::all()->keyBy('position'); // Assuming 'position' is unique for each signatory
-
-        // Define a helper function to format signatory names
-        $formatSignatoryName = function ($signatory) {
-            $profTitle = $signatory->profTitles ? strtoupper($signatory->profTitles) . ' ' : '';
-            $middleInitial = $signatory->middleName ? strtoupper($signatory->middleName[0]) . '.' : '';
-            return strtoupper($profTitle . $signatory->firstName . ' ' . $middleInitial . ' ' . $signatory->lastName);
-        };
-
-        // Use signatory data for names and positions
-        $preparedBy = $signatories->get('Project Document Specialist');
-        $reviewedBy = $signatories->get('Senior Environmental Management Specialist');
-        $checkedBy = $signatories->get('Chief, Ambient Monitoring and Forcasting Section Services');
-        $recommendedBy = $signatories->get('Chief, Environmental Documentation Station Enforcement Division');
-
-        // LastPage
-        $fpdf->AddPage();
-        //NAMES AND SIGNATURE
-        $fpdf->ln(15); //margin top (new line)
-        $fpdf->SetFont('Arial', '', 10);
-        $fpdf->Cell(5);
-        $fpdf->Cell(100, 10, 'Prepared by:', 0, 0, 'L');
-        $fpdf->Cell(20, 10, 'Reviewed by:', 0, 1, 'L');
-        $fpdf->ln(20);
-
-        $fpdf->SetFont('Arial', 'B', 12);
-        $fpdf->Cell(5);
-        // $fpdf->Cell(100, 5, 'ENGR. JOHARA JANE G. PECSON', 0, 0, 'L');
-        $fpdf->Cell(100, 5, $preparedBy ? $formatSignatoryName($preparedBy) : '', 0, 0, 'L');
-        // $fpdf->Cell(0, 5, 'JESSIE JAMES B. OSIN', 0, 1, 'L');
-        $fpdf->Cell(0, 5, $reviewedBy ? $formatSignatoryName($reviewedBy) : '', 0, 1, 'L');
-
-        $fpdf->SetFont('Arial', '', 10);
-        $fpdf->Cell(5);
-        // $fpdf->Cell(100, 5, 'Project Document Specialist', 0, 0, 'L');
-        $fpdf->Cell(100, 5, $preparedBy ? $preparedBy->position : '', 0, 0, 'L');
-        // $fpdf->Cell(0, 5, 'Senior Environmental Management Specialist', 0, 1, 'L');
-        $fpdf->Cell(0, 5, $reviewedBy ? $reviewedBy->position : '', 0, 1, 'L');
-
-        $fpdf->ln(5);
-        $fpdf->Cell(5);
-        $fpdf->Cell(100, 10, 'Checked by:', 0, 0, 'L');
-        $fpdf->Cell(0, 10, 'Recommended by:', 0, 1, 'L');
-        $fpdf->ln(20);
-
-        $fpdf->SetFont('Arial', 'B', 12);
-        $fpdf->Cell(5);
-        // $fpdf->Cell(100, 5, 'ENGR. ROSALINDA L. ILOGON', 0, 0, 'L');
-        $fpdf->Cell(100, 5, $checkedBy ? $formatSignatoryName($checkedBy) : '', 0, 0, 'L');
-        // $fpdf->Cell(0, 5, 'ENGR. DOVEE CHERRY I. GEOLLEGUE', 0, 1, 'L');
-        $fpdf->Cell(0, 5, $recommendedBy ? $formatSignatoryName($recommendedBy) : '', 0, 1, 'L');
-
-        $fpdf->SetFont('Arial', '', 10);
-        $fpdf->Cell(5);
-        $fpdf->Cell(100, 5, 'Chief, Ambient Monitoring and Forcasting Section', 0, 0, 'L');
-        // $fpdf->Cell(100, 5, $checkedBy ? $checkedBy->position : '', 0, 0, 'L');
-        $fpdf->Cell(0, 5, 'Chief, Environmental Documentation Station', 0, 1, 'L');
-        // $fpdf->Cell(0, 5, $recommendedBy ? $recommendedBy->position : '', 0, 1, 'L');
-
-        $fpdf->Cell(5);
-        $fpdf->Cell(100, 5, 'Services Section', 0, 0, 'L');
-        $fpdf->Cell(0, 5, 'Enforcement Division', 0, 1, 'L');
+        SignatorySection::SignatoryReport($fpdf);
 
         // Output PDF with a unique filename
         $today = date('Y'); // Get current year only (YYYY format)
-        $fpdf->Output('I', "AirSense $today Annual CO Assessment.pdf");
+        $fpdf->Output('I', "AirSense $today Annual PM10 Assessment.pdf");
         exit;
     }
 
